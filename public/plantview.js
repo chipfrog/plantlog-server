62d14fbf9a1a991ee25c6b2282029c112619b23d
chipfrog <chipsfrog@gmail.com>
2025-08-29 22:43:26 +0300

    start making watering canvas

--- public/plantview.js
+++ public/plantview.js
@@ -1,13 +1,44 @@
 const mainApp = document.getElementById("app")
+
+const infoView = document.getElementById("info-view")
+const careView = document.getElementById("care-view")
+
+const waterCanvas = document.getElementById("water-canvas")
+const ctx = waterCanvas.getContext("2d")
+
+waterCanvas.width = "100%"
+waterCanvas.height = "100%"
+// waterCanvas.fillStyle = "blue"
+// waterCanvas.fill()
+
+
 const waterBtn = document.getElementById("water-btn")
 const wateredText = document.getElementById("last-watered")
-
+const toggleViewBtn = document.getElementById("toggle-view-btn")
 const plantCode = JSON.parse(mainApp.dataset.plantCode)
+
+let careViewActive = false
+
+
+toggleViewBtn.addEventListener('click', (e) => {
+    e.preventDefault()
+    toggleView()
+})
 
 waterBtn.addEventListener('click', (e) => {
     e.preventDefault()
     addWatering()
 })
+
+function toggleView() {
+    if (!careViewActive) {
+        careView.style.width = "100%"
+        careViewActive = true
+    } else {
+        careView.style.width = "0"
+        careViewActive = false
+    }
+}
 
 async function addWatering() {
     const url = `/plant/${encodeURIComponent(plantCode)}/waterings`
@@ -34,4 +65,11 @@ async function addWatering() {
         alert("Failed to log watering.");
     }
     
+}
+
+
+function animateWaterJug(now) {
+
+
+    requestAnimationFrame(now)
 }
